1b2a81af8a91a3a244fa3dc34fa3d5b3f59b9d1d
GuilhermeHenrique659 <guilhermehenrique.o@hotmail.com>
2023-10-06 15:06:29 -0300

    try remove gunicorn

--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
+from config.envConfig import EnvConfig
 from module.classifeirModel.infrastructure.classifeirRoutes import classifeirRoutes
 from common.app.application import Application
 
 application = Application([classifeirRoutes])
 
-def create_app():
-    application.setup()
-    app = application.create_app()
-    app.run()
-    return app
+application.setup()
+app = application.create_app()
+app.run(port=EnvConfig.get_port(), host=EnvConfig.get_ip())
